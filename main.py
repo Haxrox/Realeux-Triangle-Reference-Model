3efaaaa2e9fdb2f617edf4232ee13ed270cba4ff
Haxdroid <2172885-Haxdroid@users.noreply.replit.com>
2023-09-14 19:53:07 +0000

    Draw inside triangle

--- main.py
+++ main.py
@@ -3,29 +3,28 @@ import math
 from decimal import *
 
 screen = [[0 for y in range(120)] for x in range(160)]
-print(len(screen), len(screen[0]))
 
-def setPixel(x, y):
-  if (x >= 0 and x < 160 and y >= 0 and y < 120):
+def setPixel(x, y, comp):
+  if (x >= 0 and x < 160 and y >= 0 and y < 120 and comp(x, y)):
     screen[x][y] = 1
     plt.axis([0, 160, 0, 120]) 
-    plt.plot(160 - x, 120 - y, 'ro')
-    print(f'screen[{x}][{y}] = 1')
+    plt.plot(x, y, 'ro')
+    # print(f'screen[{x}][{y}] = 1')
 
-def drawCircle(centre_x, centre_y, radius):
+def drawCircle(centre_x, centre_y, radius, comp):
     print(f'drawCircle({centre_x}, {centre_y}, {radius})')
     offset_y = 0
     offset_x = radius
     crit = 1 - radius
     while offset_y <= offset_x:
-        setPixel(centre_x + offset_x, centre_y + offset_y) #  -- octant 1
-        setPixel(centre_x + offset_y, centre_y + offset_x)  # -- octant 2
-        setPixel(centre_x - offset_x, centre_y + offset_y)   #-- octant 4
-        setPixel(centre_x - offset_y, centre_y + offset_x)   #-- octant 3
-        setPixel(centre_x - offset_x, centre_y - offset_y)   #-- octant 5
-        setPixel(centre_x - offset_y, centre_y - offset_x)   #-- octant 6
-        setPixel(centre_x + offset_x, centre_y - offset_y)   #-- octant 8
-        setPixel(centre_x + offset_y, centre_y - offset_x)   #-- octant 7
+        setPixel(centre_x + offset_x, centre_y + offset_y, comp) #  -- octant 1
+        setPixel(centre_x + offset_y, centre_y + offset_x, comp)  # -- octant 2
+        setPixel(centre_x - offset_x, centre_y + offset_y, comp)   #-- octant 4
+        setPixel(centre_x - offset_y, centre_y + offset_x, comp)   #-- octant 3
+        setPixel(centre_x - offset_x, centre_y - offset_y, comp)   #-- octant 5
+        setPixel(centre_x - offset_y, centre_y - offset_x, comp)   #-- octant 6
+        setPixel(centre_x + offset_x, centre_y - offset_y, comp)   #-- octant 8
+        setPixel(centre_x + offset_y, centre_y - offset_x, comp)   #-- octant 7
         offset_y = offset_y + 1
         if crit <= 0:
             crit = crit + 2 * offset_y + 1
@@ -56,13 +55,14 @@ def reuleaux(centre_x, centre_y, diameter):
   c_y3 = round(c_y3)
   
   print(f'[REULEAUX] Circle 1 - ({c_x1}, {c_y1})')
-  drawCircle(c_x1, c_y1, diameter)
+  drawCircle(c_x1, c_y1, diameter, lambda x, y : (x <= c_x3 and x >= c_x2 and y >= c_y3 and y <= c_y2))
   print(f'[REULEAUX] Circle 2 - ({c_x2}, {c_y2})')
-  drawCircle(c_x2, c_y2, diameter)
+  drawCircle(c_x2, c_y2, diameter, lambda x, y : (x >= c_x3 and x <= c_x1 and y <= c_y1 and y >= c_y3))
   print(f'[REULEAUX] Circle 3 - ({c_x3}, {c_y3})')
-  drawCircle(c_x3, c_y3, diameter)
+  drawCircle(c_x3, c_y3, diameter, lambda x, y : (x <= c_x1 and x >= c_x2 and y >= c_y1 and y >= c_y2))
   
   
 # drawCircle(80, 60, 40)
 reuleaux(80, 60, 80)
+# reuleaux(80, 60, 40)
 plt.show()
